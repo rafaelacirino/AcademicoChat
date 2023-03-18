cad2ef279b92b2a834d2646a2df8dfc9b853e827
rafaelacirino <rafaelaborbaf@gmail.com>
2023-03-18 17:36:12 -0300

    Tela de splash e ajustes no acttivity main

--- app/src/main/java/com/rafaela/academicochat/MainActivity.java
+++ app/src/main/java/com/rafaela/academicochat/MainActivity.java
@@ -2,7 +2,11 @@ package com.rafaela.academicochat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.rafaela.academicochat.activities.SignInActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
