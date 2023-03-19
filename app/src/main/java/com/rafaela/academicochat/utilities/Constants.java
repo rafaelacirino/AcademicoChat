5ccb01600f859a9ecdab5f82ebe7b335cb3dc517
rafaelacirino <rafaelaborbaf@gmail.com>
2023-03-19 15:31:36 -0300

    Sign Up ajustes usando Firestore Database

---
+++ app/src/main/java/com/rafaela/academicochat/utilities/Constants.java
@@ -0,0 +1,13 @@
+package com.rafaela.academicochat.utilities;
+
+public class Constants {
+
+    public static final String KEY_COLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
