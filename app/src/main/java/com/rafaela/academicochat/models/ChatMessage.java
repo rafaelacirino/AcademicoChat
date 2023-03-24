8e0478a5fc98403b85b3689adf410bea4f90ddbc
rafaelacirino <rafaelaborbaf@gmail.com>
2023-03-23 22:06:29 -0300

    Chat send and recieved messages

--- app/src/main/java/com/rafaela/academicochat/models/ChatMessage.java
+++ app/src/main/java/com/rafaela/academicochat/models/ChatMessage.java
@@ -1,6 +1,9 @@
 package com.rafaela.academicochat.models;
 
+import java.util.Date;
+
 public class ChatMessage {
 
     public String senderId, receiverId, message, dateTime;
+    public Date dateObject;
 }
